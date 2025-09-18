c7407511507d335df9382afa09d8b951210258a0
Gino Pio Loco <gpioloco@gmail.com>
2025-09-18 22:28:59 +0200

    add lua line config

---
+++ nvim/gipi/lua/plugins/lualine.lua
@@ -0,0 +1,36 @@
+-- ~/.config/nvim/lua/plugins/lualine.lua
+return {
+  "nvim-lualine/lualine.nvim",
+  event = "VeryLazy",
+  dependencies = { "nvim-tree/nvim-web-devicons" },
+  config = function()
+    require("lualine").setup({
+      options = {
+        theme = "horizon",
+        component_separators = { left = "", right = "" },
+        section_separators = { left = "", right = "" },
+        icons_enabled = true,
+        disabled_filetypes = {},
+      },
+      sections = {
+        lualine_a = { "mode" },
+        lualine_b = { "branch", "diff", "diagnostics" },
+        lualine_c = {
+          "filename",
+        },
+        lualine_x = { "encoding", "fileformat", "filetype" },
+        lualine_y = { "progress" },
+        lualine_z = { "location" },
+      },
+      inactive_sections = {
+        lualine_a = {},
+        lualine_b = {},
+        lualine_c = { "filename" },
+        lualine_x = { "location" },
+        lualine_y = {},
+        lualine_z = {},
+      },
+      extensions = { "quickfix", "nvim-tree" },
+    })
+  end,
+}
