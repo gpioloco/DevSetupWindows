a4c3227ac4d207ffb7ba1421d0e97dc5b63922da
Gino Pio Loco <gpioloco@gmail.com>
2025-09-18 22:20:07 +0200

    update nvim

--- nvim/gipi/lua/config/keymaps.lua
+++ nvim/gipi/lua/config/keymaps.lua
@@ -11,3 +11,6 @@ vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "Telescope find f
 vim.keymap.set("n", "<leader>fg", builtin.live_grep, { desc = "Telescope live grep" })
 vim.keymap.set("n", "<leader>fb", builtin.buffers, { desc = "Telescope buffers" })
 vim.keymap.set("n", "<leader>fh", builtin.help_tags, { desc = "Telescope help tags" })
+
+vim.keymap.set("n", "<leader>gs", require("telescope.builtin").grep_string, { desc = "Grep string under cursor" })
+vim.keymap.set("n", "<leader>fs", require("telescope.builtin").git_files, { desc = "Search git files" })
--- nvim/gipi/lua/plugins/telescope.lua
+++ nvim/gipi/lua/plugins/telescope.lua
@@ -1,4 +1,32 @@
 return {
   "nvim-telescope/telescope.nvim",
   tag = "0.1.8",
+  config = function()
+    require("telescope").setup({
+      defaults = {
+        mappings = {
+          i = {
+            ["<C-h>"] = "which_key",
+          },
+        },
+        preview = {
+          treesitter = false,
+        },
+      },
+      pickers = {
+        grep_string = {
+          -- example options:
+          only_sort_text = true,
+          shorten_path = true,
+          -- add more grep_string-specific options here
+        },
+        git_files = {
+          show_untracked = true, -- include untracked files
+        },
+      },
+      extensions = {
+        -- configure extensions here if needed
+      },
+    })
+  end,
 }
