f770926b333ba66000f84c2f6a210940e7b67cb4
Gino Pio Loco <gpioloco@gmail.com>
2025-09-20 19:34:36 +0200

    add clangd

--- nvim/gipi/lua/plugins/telescope.lua
+++ nvim/gipi/lua/plugins/telescope.lua
@@ -1,32 +1,29 @@
 return {
   "nvim-telescope/telescope.nvim",
   tag = "0.1.8",
-  config = function()
-    require("telescope").setup({
-      defaults = {
-        mappings = {
-          i = {
-            ["<C-h>"] = "which_key",
-          },
-        },
-        preview = {
-          treesitter = false,
+  opts = {
+    defaults = {
+      cwd = vim.loop.cwd(),
+      mappings = {
+        i = {
+          ["<C-h>"] = "which_key",
         },
       },
-      pickers = {
-        grep_string = {
-          -- example options:
-          only_sort_text = true,
-          shorten_path = true,
-          -- add more grep_string-specific options here
-        },
-        git_files = {
-          show_untracked = true, -- include untracked files
-        },
+      preview = {
+        treesitter = false,
       },
-      extensions = {
-        -- configure extensions here if needed
+    },
+    pickers = {
+      grep_string = {
+        only_sort_text = true,
+        shorten_path = true,
       },
-    })
-  end,
+      git_files = {
+        show_untracked = true,
+      },
+    },
+    extensions = {
+      -- configure extensions here if needed
+    },
+  },
 }
